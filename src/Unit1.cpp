0f62631d56067f2e96e546544563f7dfb44aeb96
epunch14 <epunch@emc-sq.com>
2016-10-17 08:44:22 -0400

    ImportVFTr()   line 3748 base.ELSETinputnamesCh[] refactored to fix skipping in *.wrp

--- src/Unit1.cpp
+++ src/Unit1.cpp
@@ -3743,8 +3743,11 @@ temp_cht3[strlen(chtm)-1]='\0';
 base.ELSETinputnames[nGID]=UTF8ToString(temp_cht3); //This creates a UnicodeString of 80 characters but how to "trim"?
 delete temp_cht3;temp_cht3=NULL;
 
-for(kk=0;kk<strlen(chtm);kk++)base.ELSETinputnamesCh[(79+1)*nGID+kk]=chtm[kk];
-base.ELSETinputnamesCh[(79+1)*nGID+strlen(chtm)]='\0';
+//for(kk=0;kk<strlen(chtm);kk++)base.ELSETinputnamesCh[(79+1)*nGID+kk]=chtm[kk];
+//base.ELSETinputnamesCh[(79+1)*nGID+strlen(chtm)]='\0';
+for(kk=0;kk<strlen(chtm)-1;kk++)base.ELSETinputnamesCh[(79+1)*nGID+kk]=chtm[kk];
+base.ELSETinputnamesCh[(79+1)*nGID+strlen(chtm)-1]='\0';
+
 
 
 // int bufferSize1=WideCharToMultiByte(CP_UTF8,0,base.groupsname[j].w_str(), -1,NULL,0,NULL,NULL);
